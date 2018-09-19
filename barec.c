b49c10c5c17bd04f2f2b75e35b9e59b13b33d796
Janos Vaczi <janos.vaczi@gmail.com>
2018-09-19 06:42:55 +0100

    Simplified state -> oldstate move. Added leftstate variable.

--- barec.c
+++ barec.c
@@ -2,25 +2,28 @@
 #include <ctype.h>
 #include <string.h>
 
-typedef enum {
-NORMAL, 
-STARTCOMMENT, 
-INCCOMMENT, 
-ENDCCOMMENT, 
-INSTRING, 
-ESCAPE, 
-INCPPCOMMENT, 
-INCLUDE, 
-SPACE 
+typedef enum
+{
+	NORMAL,
+	STARTCOMMENT,
+	INCCOMMENT,
+	ENDCCOMMENT,
+	INSTRING,
+	ESCAPE,
+	INCPPCOMMENT,
+	INCLUDE,
+	SPACE
 } State;
 
-typedef enum {
-FALSE,
-TRUE
+typedef enum
+{
+	FALSE,
+	TRUE
 } Bool;
 
-int iscname(int c) {
-	return isalnum(c) || c=='_';
+int iscname (int c)
+{
+	return isalnum (c) || c == '_';
 }
 
 // Skips over:
@@ -32,153 +35,269 @@ int iscname(int c) {
 // - tabs
 // Prints one space only!
 // Separate tokens: alnum -> non-alnum, non-alnum -> other non-alnum, 
-int main(int argc,char**args){
-    int c; // next char from c source
-    int prevc;  // previous char to detect token limits
-    int ststr;  // string starting character, has to match with ending char
-    State oldst=NORMAL; // old state
-    State st=NORMAL; // state;position in c source
-	Bool keephash=FALSE;  // keeping preprocessor directives lets the code to compile
-	if (argc>1)
-		keephash=(strcmp(args[1],"keephash")==0)?TRUE:FALSE;
+int main (int argc, char **args)
+{
+	int c;						// next char from c source
+	int prevc;					// previous char to detect token limits
+	int ststr;					// string starting character, has to match
 
-    while((c=getchar())!=EOF){
-        switch(st){
-            case NORMAL:
-                switch(c){
-                    // go to special cases like comment, include, string, escape
-					case'/':
-						if(oldst!=SPACE)
-							putchar(' ');
-						oldst=st;
-						st=STARTCOMMENT;
-						break;
+	// with ending char
+	State oldst = NORMAL;		// old state
+	State st = NORMAL;			// state;position in c source
+	Bool keephash = FALSE;		// keeping preprocessor directives lets the
 
-					case'#':
-						if(keephash){
-							putchar('\n');
-							putchar(c);
-							oldst=st;
-						}
-						st=INCLUDE;
-						break;
+	// code to compile
 
-                    case'"':case'\'':
-						if (oldst!=SPACE&&prevc!='L')
-							putchar(' ');
-						putchar(c);
-						oldst=st;
-						st=INSTRING;
-						ststr=c;
-						break;
+	if (argc > 1)
+		keephash = (strcmp (args[1], "keephash") == 0) ? TRUE : FALSE;
 
-                    case'\\':oldst=st;st=ESCAPE;break;
-                    // process whitespaces
-                    case'\t':case'\n':case'\r':case' ':
-						if(oldst!=SPACE)putchar(' ');
-						oldst=SPACE;
-						break;
-                    // normal chars
-                    default:  // process token limits
-                        if (oldst!=SPACE){
-							// consider adding space because there was none
-                            if(iscname(prevc)!=iscname(c)){
-                                // leave decimal point!
-								if(isdigit(prevc)&&c=='.'){} else
-								if(prevc=='.'&&isdigit(c)){} else
-								putchar(' ');
-							}
-                            if(!iscname(prevc)&&!iscname(c)){
-								if (prevc!=c) {
-									// easier to define when not to add space
-									if(prevc=='-'&&c=='>'){} else
-									if(prevc=='<'&&c=='='){} else
-									if(prevc=='>'&&c=='='){} else
-									if(prevc=='!'&&c=='='){} else
-									if(prevc=='+'&&c=='='){} else
-									if(prevc=='-'&&c=='='){} else
-									if(prevc=='*'&&c=='='){} else
-									if(prevc=='/'&&c=='='){} else
-									if(prevc=='|'&&c=='='){} else
-									if(prevc=='&'&&c=='='){} else
-									if(prevc=='^'&&c=='='){} else
-									if(prevc=='%'&&c=='='){} else
-									putchar(' ');
-								} else {
-									// easier to define when not to add space
-									if(c=='+'||c=='-'||c=='|'||c=='&'||c==':'||c=='='||c=='<'||c=='>'||c=='.'){}
-									else putchar(' ');
-								}
-							}
-                        }
-						oldst=st;
-                        putchar(c);
-                        break;
-                }
-            break;
-            case STARTCOMMENT:
-                switch(c){
-                    case'/':st=INCPPCOMMENT;break;
-                    case'*':st=INCCOMMENT;break;
-                    case'\\':putchar('/');oldst=NORMAL;st=ESCAPE;break;
-                    case' ':putchar('/');st=NORMAL;break;
-                    case'"':case'\'':putchar('/');putchar(c);st=INSTRING;ststr=c;break;
-					default:
-						putchar('/');
-						if(c!='=')putchar(' ');
-						putchar(c);
-						st=NORMAL;
-						break;
-                }
-            break;
-            case INCCOMMENT:
-                switch(c){
-                    case'*':st=ENDCCOMMENT;break;
-                    //case'\\':oldst=st;st=ESCAPE;break;
-                }
-            break;
-            case ENDCCOMMENT:
-                switch(c){
-                    case'/':st=NORMAL;break;
-                    case'*':st=ENDCCOMMENT;break;
-                    case'\\':oldst=INCCOMMENT;st=ESCAPE;break;
-                    default:st=INCCOMMENT;break;
-                }
-            break;
-            case INSTRING:
-                switch(c){
-                    case'"':case'\'':putchar(c);if(c==ststr)st=NORMAL;break;
-                    case'\\':oldst=st;st=ESCAPE;break;
-                    default:putchar(c);break;
-                }
-            break;
-            case ESCAPE:
-				st=oldst;
-				if(st==NORMAL||st==INSTRING||(st==INCLUDE&&keephash)){
-					putchar('\\');
-					putchar(c);
+	while ((c = getchar ()) != EOF)
+	{
+		State leftst = st;
+
+		switch (st)
+		{
+		case NORMAL:
+			switch (c)
+			{
+				// go to special cases like comment, include, string, escape
+			case '/':
+				st = STARTCOMMENT;
+				break;
+
+			case '#':
+				if (keephash)
+				{
+					putchar ('\n');
+					putchar (c);
 				}
-            break;
-            case INCPPCOMMENT:
-                switch(c){
-                    case'\r':case'\n':oldst=SPACE;st=NORMAL;break;
-                }
-            break;
-            case INCLUDE:
-                switch(c){
-					case'\\':oldst=INCLUDE;st=ESCAPE;break;
-                    case'\r':case'\n':
-						st=NORMAL;
-						if(keephash){
-							putchar(c);
-							oldst=SPACE;
-						}
-						break;
+				st = INCLUDE;
+				break;
 
-					default:if(keephash)putchar(c);
-                }
-            break;
-        }  // switch(st)
-        prevc=c;
-    }  // while((c=getchar()!=EOF)
+			case '"':
+			case '\'':
+				if (oldst != SPACE && prevc != 'L')
+					putchar (' ');
+				putchar (c);
+				st = INSTRING;
+				ststr = c;
+				break;
+
+			case '\\':
+				st = ESCAPE;
+				break;
+				// process whitespaces
+			case '\t':
+			case '\n':
+			case '\r':
+			case ' ':
+				if (oldst != SPACE)
+					putchar (' ');
+				leftst = SPACE;
+				break;
+				// normal chars
+			default:			// process token limits
+				if (oldst != SPACE)
+				{
+					// consider adding space because there was none
+					if (iscname (prevc) != iscname (c))
+					{
+						// leave decimal point!
+						if (isdigit (prevc) && c == '.')
+						{
+						}
+						else if (prevc == '.' && isdigit (c))
+						{
+						}
+						else
+						{
+							putchar (' ');
+						}
+					}
+					if (!iscname (prevc) && !iscname (c))
+					{
+						if (prevc != c)
+						{
+							// easier to define when not to add space
+							if (prevc == '-' && c == '>')
+							{
+							}
+							else if (prevc == '<' && c == '=')
+							{
+							}
+							else if (prevc == '>' && c == '=')
+							{
+							}
+							else if (prevc == '!' && c == '=')
+							{
+							}
+							else if (prevc == '+' && c == '=')
+							{
+							}
+							else if (prevc == '-' && c == '=')
+							{
+							}
+							else if (prevc == '*' && c == '=')
+							{
+							}
+							else if (prevc == '/' && c == '=')
+							{
+							}
+							else if (prevc == '|' && c == '=')
+							{
+							}
+							else if (prevc == '&' && c == '=')
+							{
+							}
+							else if (prevc == '^' && c == '=')
+							{
+							}
+							else if (prevc == '%' && c == '=')
+							{
+							}
+							else
+							{
+								putchar (' ');
+							}
+						}
+						else
+						{
+							// easier to define when not to add space
+							if (c == '+' || c == '-' || c == '|'
+								|| c == '&' || c == ':' || c == '='
+								|| c == '<' || c == '>' || c == '.')
+							{
+							}
+							else
+							{
+								putchar (' ');
+							}
+						}
+					}
+				}
+				putchar (c);
+				break;
+			}
+			break;
+
+		case STARTCOMMENT:
+			switch (c)
+			{
+			case '/':
+				st = INCPPCOMMENT;
+				break;
+			case '*':
+				st = INCCOMMENT;
+				break;
+			case '\\':
+				putchar ('/');
+				leftst = NORMAL;
+				st = ESCAPE;
+				break;
+			case ' ':
+				putchar ('/');
+				st = NORMAL;
+				break;
+			case '"':
+			case '\'':
+				putchar ('/');
+				putchar (c);
+				st = INSTRING;
+				ststr = c;
+				break;
+			default:
+				putchar ('/');
+				if (c != '=')
+					putchar (' ');
+				putchar (c);
+				st = NORMAL;
+				break;
+			}
+			break;
+		case INCCOMMENT:
+			switch (c)
+			{
+			case '*':
+				st = ENDCCOMMENT;
+				break;
+			}
+			break;
+		case ENDCCOMMENT:
+			switch (c)
+			{
+			case '/':
+				st = NORMAL;
+				break;
+			case '*':
+				st = ENDCCOMMENT;
+				break;
+			case '\\':
+				leftst = INCCOMMENT;
+				st = ESCAPE;
+				break;
+			default:
+				st = INCCOMMENT;
+				break;
+			}
+			break;
+		case INSTRING:
+			switch (c)
+			{
+			case '"':
+			case '\'':
+				putchar (c);
+				if (c == ststr)
+					st = NORMAL;
+				break;
+			case '\\':
+				st = ESCAPE;
+				break;
+			default:
+				putchar (c);
+				break;
+			}
+			break;
+		case ESCAPE:
+			st = oldst;
+			if (st == NORMAL || st == INSTRING || (st == INCLUDE && keephash))
+			{
+				putchar ('\\');
+				putchar (c);
+			}
+			break;
+		case INCPPCOMMENT:
+			switch (c)
+			{
+			case '\r':
+			case '\n':
+				leftst = SPACE;
+				st = NORMAL;
+				break;
+			}
+			break;
+		case INCLUDE:
+			switch (c)
+			{
+			case '\\':
+				st = ESCAPE;
+				break;
+			case '\r':
+			case '\n':
+				st = NORMAL;
+				if (keephash)
+				{
+					putchar (c);
+					leftst = SPACE;
+				}
+				break;
+
+			default:
+				if (keephash)
+					putchar (c);
+			}
+			break;
+		}						// switch(st)
+		prevc = c;
+		oldst = leftst;
+	}							// while((c=getchar()!=EOF)
 }
